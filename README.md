cd494312d2138f08b7d7da62306def37886900a4
Jon Stevens <latchkey@gmail.com>
2015-05-02 16:12:45 -0700

    initial add

---
+++ README.md
@@ -0,0 +1,5 @@
+= Enumerate-Angular
+
+Simple little angular directive that shows a nice number on the screen.
+
+![enumerate](demo.png)
---
+++ enumerate.js
@@ -0,0 +1,52 @@
+(function(angular) {
+
+	var enumerate = angular.module('enumerate-angular', []);
+
+	enumerate.directive('enumerate', function() {
+		return {
+			restrict: 'C',
+			scope: {
+				value: '=',
+				size: '='
+			},
+			replace: false,
+			controller: ['$scope', function($scope) {
+
+				if (!$scope.size) {
+					$scope.size = 'enumerate-large';
+				}
+
+				var processValue = function(value) {
+					var splitData = [];
+
+					for (var i of value.split("")) {
+						var style = null;
+
+						if (i === ',' || i === '.') {
+							style = 'punc';
+						} else if (i === '$') {
+							style = 'money';
+						}
+
+						splitData.push({
+							char: i,
+							style: style
+						})
+					}
+
+					return splitData;
+				};
+
+				$scope.$watch('value', function() {
+					$scope.cols = processValue($scope.value);
+				});
+			}],
+			template:
+				'<div ng-repeat="col in cols" ng-class="[size, col.style]">' +
+					'<span ng-bind="col.char"></span>' +
+				'</div>'
+		};
+	});
+
+})(angular);
+
